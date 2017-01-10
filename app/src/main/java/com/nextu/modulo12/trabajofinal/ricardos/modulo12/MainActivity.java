b0017057938496707975e56f2b62bdc506ba877d
RicardoS <lsalazarr@gmail.com>
2017-01-09 23:40:46 -0500

    Banner LISTO

--- app/src/main/java/com/nextu/modulo12/trabajofinal/ricardos/modulo12/MainActivity.java
+++ app/src/main/java/com/nextu/modulo12/trabajofinal/ricardos/modulo12/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Base64;
@@ -14,16 +15,26 @@ import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.ads.doubleclick.PublisherAdRequest;
+import com.google.android.gms.ads.doubleclick.PublisherAdView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
     private CallbackManager cM;
     private LoginButton lB;
+    private PublisherAdView publisherAdView;
+    String nombre,email,foto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,14 +48,51 @@ public class MainActivity extends AppCompatActivity {
         getFbKeyHash("2SUkDCEM6T/vKAAmolnu5Vbcpbg=");
 
         setContentView(R.layout.activity_main);
-
+        publisherAdView = (PublisherAdView) findViewById(R.id.ad_view);
+        PublisherAdRequest adRequest = new PublisherAdRequest.Builder().build();
+        publisherAdView.loadAd(adRequest);
         lB = (LoginButton)findViewById(R.id.login_facebook);
+
+        lB.setReadPermissions(Arrays.asList(
+                "public_profile", "email", "user_birthday", "user_friends"));
 
         lB.registerCallback(cM, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
 
                 Toast.makeText(MainActivity.this, "¡Inicio de sesión exitoso!", Toast.LENGTH_LONG).show();
+
+                GraphRequest request = GraphRequest.newMeRequest(
+                        loginResult.getAccessToken(),
+                        new GraphRequest.GraphJSONObjectCallback() {
+                            @Override
+                            public void onCompleted(
+                                    JSONObject object,
+                                    GraphResponse response) {
+                                Log.v("LoginActivity Response ", response.toString());
+
+                                try {
+                                    nombre = object.getString("name");
+
+                                    email = object.getString("email");
+                                    foto =  object.getString("public_profile");
+                                    Intent intent = new Intent(MainActivity.this,segunda_Pantalla.class);
+                                    intent.putExtra("email",email);
+                                    intent.putExtra("foto",foto);
+                                    intent.putExtra("nombre",nombre);
+                                    startActivity(intent);
+
+
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        });
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,name,email,gender, birthday,public_profile");
+                request.setParameters(parameters);
+                request.executeAsync();
+
 
             }
 
@@ -86,4 +134,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int reqCode, int resCode, Intent i){
         cM.onActivityResult(reqCode, resCode, i);
     }
+
+    @Override
+    protected void onDestroy() {
+        if(publisherAdView != null){
+            publisherAdView.destroy();
+        }
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(publisherAdView != null){
+            publisherAdView.resume();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        if(publisherAdView != null){
+            publisherAdView.pause();
+        }
+        super.onPause();
+    }
+
+
 }
---
+++ app/src/main/java/com/nextu/modulo12/trabajofinal/ricardos/modulo12/segunda_Pantalla.java
@@ -0,0 +1,13 @@
+package com.nextu.modulo12.trabajofinal.ricardos.modulo12;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class segunda_Pantalla extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_segunda__pantalla);
+    }
+}
